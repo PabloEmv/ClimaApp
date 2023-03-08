415065a7164cdcd23edaec1cb5faf56f1bb6c50f
Pablo Martínez <pablo.ed.martinez@gmail.com>
2023-03-08 16:53:37 -0300

    corrección del fondo

--- src/main.jsx
+++ src/main.jsx
@@ -6,26 +6,12 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
-import { CssBaseline, GlobalStyles } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import { SnackbarProvider } from "notistack";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyles
-        styles={{
-          body: {
-            backgroundImage: 'url(./src/assets/Cloudy.svg)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          },
-          '@media screen and (max-width: 600px)': {
-            body: {
-              backgroundPosition: 'right center',
-            },
-          },
-        }}
-      />
     <SnackbarProvider>
       <CssBaseline />
       <App />
