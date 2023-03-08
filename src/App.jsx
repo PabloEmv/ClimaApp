12657563343d89271b5406b182d13cf186a851fd
Pablo Martínez <pablo.ed.martinez@gmail.com>
2023-03-08 16:54:09 -0300

    corrección de petición

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 import WeatherResult from "./components/WeatherResult";
 
-const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${
   import.meta.env.VITE_API_KEY
 }&lang=es&q=`;
 
